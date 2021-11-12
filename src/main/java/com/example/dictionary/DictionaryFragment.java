aef9b5972fc0a3a9f1179d2119e11b4f9f909831
ngoctrinh49 <ngoctrinhmai040920@gmail.com>
2021-11-12 14:50:18 +0700

    add step4: listview + click word

--- src/main/java/com/example/dictionary/DictionaryFragment.java
+++ src/main/java/com/example/dictionary/DictionaryFragment.java
@@ -1,15 +1,15 @@
 package com.example.dictionary;
 
 import android.os.Bundle;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class DictionaryFragment extends Fragment {
 
@@ -27,18 +27,31 @@ public class DictionaryFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_dictionary, container, false);
     }
 
+    //ham xu ly listview
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        Button myButton = (Button) view.findViewById(R.id.myBtn);
-        myButton.setOnClickListener(new View.OnClickListener() {
+
+        ListView words = view.findViewById(R.id.dictionaryList);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, getListOfWords());
+        words.setAdapter(adapter);
+        words.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 if (fragmentListener != null) {
-                    fragmentListener.onItemClick(message);
+                    fragmentListener.onItemClick(getListOfWords()[position]);       //hien message ten tu vua click
                 }
             }
         });
+    }
+
+    public String[] getListOfWords() {
+        String[] words = new String[] {
+                "a",
+                "b",
+                "c"
+        };
+        return words;
     }
 
     public void setOnFragmentListener(FragmentListener fragmentListener) {
